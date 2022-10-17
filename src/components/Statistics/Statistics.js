fb20a3312fe97d4f39fc545671895448e8e81c44
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-18 04:24:34 +0600

    charts added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from 'recharts';
 
-const Statistics = () => {
+function Statistics() {
+  const { data } = useLoaderData();
   return (
-    <div>
-      <h4 className='font-semibold'> This is Statistics page</h4>
+    <div className='container min-h-screen pt-16 flex justify-center'>
+      <LineChart
+        width={600}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type='monotone' dataKey='total' stroke='#8884d8' />
+        <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
+        <XAxis dataKey='name' />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
     </div>
   );
-};
+}
 
 export default Statistics;
