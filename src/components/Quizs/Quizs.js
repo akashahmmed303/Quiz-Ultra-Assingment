00ad94aee8ebad561fd6fd9f1dbaa0b8afe7c89d
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-18 03:41:49 +0600

    toast show

--- src/components/Quizs/Quizs.js
+++ src/components/Quizs/Quizs.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import Correct from '../Correct/Correct';
 import InCorrect from '../InCorrect/InCorrect';
 import QuizCart from '../QuizCart/QuizCart';
@@ -28,11 +28,59 @@ function Quizs() {
   }, []);
 
   const handleCliked = async (text, key) => {
-    console.log(handleCliked);
+    const obj = {
+      ...selected,
+    };
+    obj[key] = text;
+    // if(selected.obj.)
+    await setSelected({ ...obj });
+
+    const isCorrect = correctAnswers.find(elem => elem === text);
+    const isSelected = Object.values(selected).includes(text);
+    const correctInclude = correctAnswers.includes(selected[key]);
+    if (isCorrect) {
+      toast.success('Correct!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+    } else {
+      toast.error('Incorrect!', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+    }
+    if (isCorrect && isSelected !== true) {
+      setCorrect(prev => prev + 1);
+      console.log(true);
+    } else if (correctInclude === true && !isCorrect) {
+      console.log('minus');
+      setCorrect(prev => prev - 1);
+    }
   };
 
-  const handleSee = ({ correctAnswer }) => {
-    console.log(handleSee);
+  const handleSee = correctAnswer => {
+    toast.success(`${correctAnswer}`, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
   };
 
   return (
@@ -41,19 +89,21 @@ function Quizs() {
         {name}
       </h1>
       <div className='grid lg:grid-cols-2 lg:gap-x-8 gap-y-8 '>
-        {questions.map(({ question, options, id, correctAnswer }) => {
-          return (
-            <QuizCart
-              handleSee={handleSee}
-              correctAnswer={correctAnswer}
-              options={options}
-              question={question}
-              id={id}
-              key={id}
-              handleCliked={handleCliked}
-            ></QuizCart>
-          );
-        })}
+        {questions.map(
+          ({ question, options, id, correctAnswer }, quesIndex) => {
+            return (
+              <QuizCart
+                handleSee={handleSee}
+                correctAnswer={correctAnswer}
+                options={options}
+                question={question}
+                id={id}
+                key={id}
+                handleCliked={handleCliked}
+              ></QuizCart>
+            );
+          }
+        )}
       </div>
       <Correct correct={correct}></Correct>
       <ToastContainer />
